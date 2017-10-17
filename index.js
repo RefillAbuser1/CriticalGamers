020e0d42099a8b57157fc7c8b123ad214981112e
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 04:30:24 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const config = require("./config.json");
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+
+bot.user.setPresence({game: {name: 'prefix: == | ==help', type:0 } });
+});
+
+bot.on('message', message => {
+  if(message.author.bot) return;
+  if(!message.content.startsWith(config.prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "add") {
+    let numArry = args.map(n=> parseInt(n));
+    let total = numArry.reduce( (p, c) => p+c);
+
+    message.channel.sendMessage(total);
+  } 
+
+  if (command === "say") {
+    message.channel.sendMessage(args.join(" "));
+  } else
+
+  if (command === "invite") {
+    message.channel.sendMessage("Invite: https://discord.gg/r4aBuTw");
+  } 
+
+  if (command === "donate") {
+    message.channel.sendMessage("Help CriticalGamers stay online! | paypal.me/ServerSupport |");
+  } 
+	  
+});
+
+bot.login(process.env.BOT_TOKEN);
